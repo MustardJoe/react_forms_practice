b77fa1de4d9ef2be7d8eb54b56962bfbc4033f95
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-18 23:20:41 -0700

    refactored form component

--- src/components/FormThing/FormThing.js
+++ src/components/FormThing/FormThing.js
@@ -2,33 +2,10 @@ import React, { Component } from 'react';
 
 class FormThing extends Component {
   state = {
-    inputText: 'wrap',
-    outputText: 'hi',
+    inputText: '',
+    outputText: '',
   }
-
-  // handleSubmit = event => {
-  //   event.preventDefault();
-  //   /* eslint-disable no-console */
-  //   console.log('this state:', this.state.inputText);
-  //   this.setState.outputText = this.state.inputText;
-  //   console.log('after: ', this.state);
-  // }
-
-
-  handleTextBox = event => {
-    this.setState({ inputText: event.target.value });
-    // this.setState(outputText): inputText;
-    this.updateOutputText();
-  }
-
-  // incrementCount = () => {
-  //   this.setState(state => {
-  //     return {
-  //       count: state.count + 1
-  //     };
-  //   });
-  // }
-
+  
   updateOutputText = () => {
     this.setState(state => {
       return {
@@ -36,6 +13,13 @@ class FormThing extends Component {
       };
     });
   }
+
+  handleTextBox = event => {
+    this.setState({ inputText: event.target.value });
+    // this.setState(outputText): inputText;
+    this.updateOutputText();
+  }
+  
 
   render() {
     const {
@@ -45,20 +29,13 @@ class FormThing extends Component {
 
     return (
       <>
-        <h1>Dumb Quiz Practice - actually I think quizes are usefull, but please
-          don`t tell anybody!
-        </h1>
         <input name="textBox" value={inputText} onChange={this.handleTextBox} />
 
         <p>{outputText}</p>
         <p>Output text should be above/before here</p>
-
       </>
     );
   }
 }
-
-/* eslint-disable no-console */
-console.log(FormThing);
 
 export default FormThing;
