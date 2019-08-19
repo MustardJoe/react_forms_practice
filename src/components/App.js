71a1027011892cb04b13963040da9eb1e7443dae
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-18 23:20:24 -0700

    refactored app

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,11 @@ import FormThing from './FormThing/FormThing';
 
 export default function App() {
   return (
-    <FormThing />
+    <>
+      <h1>Dumb Quiz Practice - actually I think quizes are usefull, but please
+          don`t tell anybody!</h1>
+      <FormThing />
+      <FormThing />
+    </>  
   );
 }
