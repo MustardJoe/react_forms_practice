83f1bf9f6d75cfcb4c6c4ee02965ad41bd6763a7
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-18 20:43:59 -0700

    have app where I need it i think

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import FormThing from './FormThing/FormThing';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <FormThing />
+  );
 }
-  
